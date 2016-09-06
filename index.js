8d16f5c5510d2bf49dc958c2dcc0d911508e966b
N.S.Devaraj <nsdevaraj@gmail.com>
2016-09-06 15:13:26 +0530

    auto port

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var path = require('path');
 var express = require('express');
 
 var app = express();
-
+var port = process.env.PORT || 8080;
 var staticPath = path.join(__dirname, '/public');
 app.use(express.static(staticPath));
 
-app.listen(3000, function() {
-  console.log('listening');
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
